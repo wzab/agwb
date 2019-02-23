17c6ecd7d7595b7c55df4cb91abddc26a392478e
Wojciech M. Zabolotny <wzab01@gmail.com>
2019-02-23 01:16:50 +0100

    Updated example test2 to run with multimaster version.

--- test2/generate.sh
+++ test2/generate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 mkdir -p src/created/gen
-( cd src/created ; ../../../src/addr_gen_wb.py example1.xml gen gen )
+( cd src/created ; ../../../src/addr_gen_wb.py --infile example1.xml --hdl gen --ipbus gen --fs gen )
