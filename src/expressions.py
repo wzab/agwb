b3c684e8ec1ef01fc79c3ff44f23f89a7fd3492b
Wojciech Zabołotny <wzab@wzdell.nasz.dom>
2019-05-24 12:21:14 +0200

    Switched to the safer ast-based evaluator.

--- src/expressions.py
+++ src/expressions.py
@@ -1,10 +1,31 @@
-""" This is the simplest implemantation of the module "expressions"
-It uses the "eval" function that is unsafe.
-A better implementation should be available soon.
+"""This code is based on the post from stack overflow:
+    https://stackoverflow.com/a/30516254/1735409
+   In Debian it requires that you install python3-asteval package
 """
+import ast, math
 
-locals = {}
-def exprval(valstr):
-    return eval(valstr,None,locals)
+locals =  {key: value for (key,value) in vars(math).items() if key[0] != '_'}
+locals.update({"abs": abs, "complex": complex, "min": min, "max": max, "pow": pow, "round": round})
 def addval(valname,valstr):
     locals[valname]=exprval(valstr)
+
+class Visitor(ast.NodeVisitor):
+    def visit(self, node):
+       if not isinstance(node, self.whitelist):
+           raise ValueError(node)
+       return super().visit(node)
+
+    whitelist = (ast.Module, ast.Expr, ast.Load, ast.Expression, ast.Add, ast.Sub, ast.UnaryOp, ast.Num, ast.BinOp,
+            ast.Mult, ast.Div, ast.Pow, ast.BitOr, ast.BitAnd, ast.BitXor, ast.USub, ast.UAdd, ast.FloorDiv, ast.Mod,
+            ast.LShift, ast.RShift, ast.Invert, ast.Call, ast.Name)
+
+
+def exprval(expr):
+    if any(elem in expr for elem in '\n#') : raise ValueError(expr)
+    try:
+        node = ast.parse(expr.strip(), mode='eval')
+        Visitor().visit(node)
+        return eval(compile(node, "<string>", "eval"), {'__builtins__': None}, locals)
+    except Exception: raise ValueError(expr)
+
+
