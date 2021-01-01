fa9ce1b219d04eafa98f75d096d570cdfe16c76c
Wojciech M. Zabolotny <wzab01@gmail.com>
2021-01-01 19:22:59 +0100

    Moved test designs to the "tests" directory, so that the main direcory is less messy.

--- test2/generate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-mkdir -p src/created/gen
-( cd src/created ; ../../../src/addr_gen_wb.py --infile example1.xml --hdl gen --ipbus gen --fs gen )
---
+++ tests/test/generate.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+mkdir -p gen
+../../src/addr_gen_wb.py --infile example1.xml --fs ./gen --hdl ./gen --ipbus ./gen --header ./gen --python ./python_raw --html ./gen
+
+
