48e03727248cb72b6f2bc3e7683011a381c43c27
Wojciech M. Zabolotny <wzab01@gmail.com>
2021-01-05 17:29:19 +0100

    Test designs modified for compatibility with agwb_pkg.vhd generated in the output VHDL directory (instead of being statically delivered in the agwb files).

--- tests/test/generate.sh
+++ tests/test/generate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 mkdir -p gen
-../../src/addr_gen_wb.py --infile example1.xml --fs ./gen --hdl ./gen  --ipbus ./ipbusxml --amapxml ./gen_amap --header ./gen --python ./python_raw --html ./gen
+../../src/addr_gen_wb.py --infile example1.xml --fs ./gen --hdl ./gen  --ipbus ./ipbusxml --amapxml ./gen_amap --header ./gen --python ./python_raw --html ./gen --fusesoc 
 
 
