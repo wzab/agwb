ec59ba065e0133ef1aa0528c7d09a39c9d0d68c0
Ingo Froehlich <i.froehlich@gsi.de>
2021-06-03 16:36:54 +0200

    added wb core file and tcl wrapper

---
+++ hdl/wb_cdc/wb_cdc_wrapper.tcl
@@ -0,0 +1,4 @@
+set_property SCOPED_TO_REF wb_cdc [get_files wb_cdc.tcl]
+set_property PROCESSING_ORDER "LATE" [get_files wb_cdc.tcl]
+set_property USED_IN_IMPLEMENTATION "1" [get_files wb_cdc.tcl]
+set_property USED_IN_SYNTHESIS "0" [get_files wb_cdc.tcl]
